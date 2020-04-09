42afca32543d48eb9009148e398fe9507b817bf0
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-04-09 12:35:17 +0300

    Replaced var with let and const ^^

--- front_end/modals-scripts.js
+++ front_end/modals-scripts.js
@@ -1,12 +1,12 @@
 export class Modal {
   static init() {
-    var modalFilter = document.getElementById("modal-filter");
-    var buttonFilter = document.getElementById("fillter-button");
-    var closeFilter = document.getElementsByClassName("close")[0];
-    var closeDownload = document.getElementsByClassName("close")[1];
-    var buttonDownload = document.getElementById("download-button");
-    var modalDownload = document.getElementById("modal-download");
-    var pageWrapper = document.getElementById("page-wrapper");
+    const modalFilter = document.getElementById("modal-filter");
+    const buttonFilter = document.getElementById("fillter-button");
+    const closeFilter = document.getElementsByClassName("close")[0];
+    const closeDownload = document.getElementsByClassName("close")[1];
+    const buttonDownload = document.getElementById("download-button");
+    const modalDownload = document.getElementById("modal-download");
+    const pageWrapper = document.getElementById("page-wrapper");
 
     buttonFilter.onclick = function () {
       modalFilter.style.display = "block";
