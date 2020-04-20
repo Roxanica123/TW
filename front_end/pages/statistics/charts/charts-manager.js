a4cc6101168667d2db093f47298da126f22f4917
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-04-20 20:55:31 +0300

    Add style to change chart type buttons

--- front_end/pages/statistics/charts/charts-manager.js
+++ front_end/pages/statistics/charts/charts-manager.js
@@ -15,27 +15,16 @@ export class ChartsManager {
             const chartContainer = document.createElement('chart-container');
             emptyContainer.appendChild(chartContainer);
             const chartElement = document.createElement('chart');
-            const text = document.createElement('p');
-            text.innerHTML = element.text;
-            if (index % 2 == 0) {
-                chartContainer.appendChild(chartElement);
-                this.insertSwich(chartContainer, element.id);
-                chartContainer.appendChild(text);
-            }
-            else {
-                chartContainer.appendChild(text);
-                this.insertSwich(chartContainer, element.id);
-                chartContainer.appendChild(chartElement);
-            }
+            chartContainer.appendChild(chartElement);
+            this.insertChartInfo(chartContainer, element, index);
             chartElement.setAttribute('name', element.id);
             let pos = ChartsManager.chartsList.indexOf(ChartsManager.chartsList.find(chart => chart.id == element.id));
-            console.log(pos);
             if (pos > -1) {
                 ChartsManager.chartsList[pos] = new Chart(chartElement, ChartsManager.chartsList[pos].getType(), element.id);
             }
             else {
                 this.chartsList.push(new Chart(chartElement, 0, element.id));
-                pos=this.chartsList.length-1;
+                pos = this.chartsList.length - 1;
             }
 
             this.chartsList[pos].initDataTable(element.data.columns, element.data.rows);
@@ -45,18 +34,32 @@ export class ChartsManager {
     }
 
     static insertSwich(chartContainer, id) {
-        const button = document.createElement('div');
-        button.setAttribute('class', 'btn-three');
+
+        const button = document.createElement('switch');
         button.setAttribute('id', id);
-        button.innerText = 'Switch!';
+        button.innerText = 'Switch chart type!';
         chartContainer.appendChild(button);
         button.addEventListener("click", function () {
             ChartsManager.switchChart(this);
         });
+
     }
     static switchChart(switchElement) {
         const chart = ChartsManager.chartsList.find(chart => chart.id == switchElement.getAttribute('id'));
         chart.changeType();
     }
-
+    static insertChartInfo(chartContainer, element, index) {
+        const info_container = document.createElement('chart-info-container');
+        const text = document.createElement('p');
+        text.innerHTML = element.text;
+        info_container.appendChild(text);
+        this.insertSwich(info_container, element.id);
+        chartContainer.appendChild(info_container);
+        /*if (index % 2 == 0) {
+            chartContainer.appendChild(info_container);
+        }
+        else {
+            chartContainer.prepend(info_container);
+        }*/
+    }
 }
