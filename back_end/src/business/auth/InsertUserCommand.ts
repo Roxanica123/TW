bea85c579dde4600f735f03e27f3dedf290c4f86
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-06-12 12:40:31 +0300

    Save password as hash in the database

--- back_end/src/business/auth/InsertUserCommand.ts
+++ back_end/src/business/auth/InsertUserCommand.ts
@@ -1,6 +1,7 @@
 import { IUserRepository } from "../../domain/repositories"
 import { IUser } from "../../domain/entities";
 import { UserRepository } from "../../persistence/repositories";
+import * as crypto from "crypto"
 import * as jwt from "jsonwebtoken";
 
 const secret = 'secret discret:)';
@@ -17,7 +18,11 @@ export class InsertUserCommand {
         const password = this.body.password;
         const email = this.body.email;
 
-        let user = await this.userRepository.insertUser(username, email, password);
+        const hmac = crypto.createHmac('sha256', secret);
+
+        hmac.update(password);
+
+        let user = await this.userRepository.insertUser(username, email, hmac.digest('hex'));
         if (user) {
             let passwordToken = jwt.sign({ username: user.username, email: user.email }, secret);
             return passwordToken;
