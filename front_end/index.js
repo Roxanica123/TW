c982a5e67d41ea842bb637b20e16b74c5c3376a4
Sabina Apostol <sabina.bubu@yahoo.com>
2020-06-20 18:45:38 +0300

    login+register

--- front_end/index.js
+++ front_end/index.js
@@ -1,13 +1,24 @@
 var http = require('http')
+const forward = require('http-forward')
 var serveStatic = require('serve-static')
- 
+
 // Serve up public/ftp folder
 var serve = serveStatic('./', { 'index': ['pages/main-map/main-map.html'] })
- 
+
 // Create server
-var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, () => {})
+var server = http.createServer(function onRequest(req, res) {
+  req.forward = { target: 'http://localhost:5000' }
+  if (req.url.includes('.css') ||
+    req.url.includes('.js') ||
+    req.url.includes('.html') ||
+    req.url.includes('.png') ||
+    req.url.includes('.ico')) {
+    serve(req, res, () => { })
+  }
+  else {
+    forward(req, res)
+  }
 })
- 
+
 // Listen
 server.listen(3000)
