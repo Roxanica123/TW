b809f4bbf7396928e2611c934e4b9f1dbdfdd8a2
Sabina Apostol <sabina.bubu@yahoo.com>
2020-06-23 17:21:34 +0300

    modify insert

--- back_end/src/presentation/AccidentsController.ts
+++ back_end/src/presentation/AccidentsController.ts
@@ -26,58 +26,8 @@ export class AccidentsController {
     @HttpPost('/')
     public async post(_query: any, body: IAccident) {
        
-        let repository = new AccidentsRepository
-        const id = body.id
-        const source =  body.source
-        const tmc = body.tmc
-        const severity = body.severity
-        const start_time = body.start_time
-        const end_time = body.end_time
-        const start_latitude = body.start_latitude
-        const start_longitude = body.start_longitude
-        const end_latitude = body.end_latitude
-        const end_longitude = body.end_longitude
-        const distance = body.distance
-        const description = body.description
-        const number = body.number
-        const street = body.street
-        const side = body.side
-        const city = body.city
-        const county = body.county
-        const state = body.state
-        const zipcode = body.zipcode
-        const country = body.country
-        const timezone = body.timezone
-        const airport_code = body.airport_code
-        const weather_timestamp = body.weather_timestamp
-        const temperature = body.temperature
-        const wind_chill = body.wind_chill
-        const humidity = body.humidity
-        const pressure = body.pressure
-        const visibility = body.visibility
-        const wind_direction = body.wind_direction
-        const wind_speed = body.wind_speed
-        const precipitation = body.precipitation
-        const weather_condition = body.weather_condition
-        const amenity = body.amenity
-        const bump = body.bump
-        const crossing = body.crossing
-        const give_way = body.give_way
-        const junction = body.junction
-        const no_exit = body.no_exit
-        const railway = body.railway
-        const roundabout = body.roundabout
-        const station = body.station
-        const stop = body.stop
-        const traffic_calming = body.traffic_calming
-        const traffic_signal = body.traffic_signal
-        const turning_loop = body.turning_loop
-        const sunrise_sunset = body.sunrise_sunset
-        const civil_twilight = body.civil_twilight
-        const nautical_twilight = body.nautical_twilight
-        const astronomical_twilight = body.astronomical_twilight
-  
-        const accident = await repository.insertAccident( id, source, tmc, severity, start_time, end_time, start_latitude, start_longitude, end_latitude, end_longitude, distance, description, number, street, side, city, county, state, zipcode, country, timezone, airport_code, weather_timestamp, temperature, wind_chill, humidity, pressure, visibility, wind_direction, wind_speed, precipitation, weather_condition, amenity, bump, crossing, give_way, junction, no_exit, railway, roundabout, station, stop, traffic_calming, traffic_signal, turning_loop, sunrise_sunset, civil_twilight, nautical_twilight, astronomical_twilight);
+        let repository = new AccidentsRepository();
+        const accident = await repository.insertAccident( body.id, body.source, body.tmc, body.severity, body.start_time, body.end_time, body.start_latitude, body.start_longitude, body.end_latitude, body.end_longitude, body.distance, body.description, body.number, body.street, body.side, body.city, body.county, body.state, body.zipcode, body.country, body.timezone, body.airport_code, body.weather_timestamp, body.temperature, body.wind_chill, body.humidity, body.pressure, body.visibility, body.wind_direction, body.wind_speed, body.precipitation, body.weather_condition, body.amenity, body.bump, body.crossing, body.give_way, body.junction, body.no_exit, body.railway, body.roundabout, body.station, body.stop, body.traffic_calming, body.traffic_signal, body.turning_loop, body.sunrise_sunset, body.civil_twilight, body.nautical_twilight, body.astronomical_twilight);
 
         if (accident)
             return new Ok(JSON.stringify({ 'massage': 'New accident inserted' }));
